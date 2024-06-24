b8324fab31c08bff08ac0e187c86e0c4aa89c989
jhon varon <jhonvaronyate@hotmail.com>
2024-06-24 11:46:21 -0500

    create recent transactions,BankInfo and TransactionTable components to display all info about account

---
+++ components/CategoryBadge.tsx
@@ -0,0 +1,17 @@
+import { transactionCategoryStyles } from "@/constants";
+import { cn } from "@/lib/utils";
+
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const { borderColor, backgroundColor, textColor, chipBackgroundColor } =
+    transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] ||
+    transactionCategoryStyles.default;
+
+  return (
+    <div className={cn("category-badge", borderColor, chipBackgroundColor)}>
+      <div className={cn("size-2 rounded-full", backgroundColor)} />
+      <p className={cn("text-[12px] font-medium", textColor)}>{category}</p>
+    </div>
+  );
+};
+
+export default CategoryBadge;
