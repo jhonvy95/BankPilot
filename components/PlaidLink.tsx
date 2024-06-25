acc0fb9cc3b8190a5ab5a70828ce038445c10830
jhon varon <jhonvaronyate@hotmail.com>
2024-06-25 12:32:58 -0500

    Connect Multiple Bank Accounts

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -5,6 +5,7 @@ import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from "react-plaid-
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import { createLinkToken, exchangePublicToken } from "@/lib/actions/user.actions";
+import Image from "next/image";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
@@ -43,10 +44,14 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
           Connect bank
         </Button>
       ) : variant === "ghost" ? (
-        <Button variant="ghost">Connect bank</Button>
+        <Button variant="ghost" onClick={() => open()} className="plaidlink-ghost">
+          <Image src="/icons/connect-bank.svg" alt="connect bank" width={24} height={24} />
+          <p className="hidden text-[16px] font-semibold text-black-2 xl:block">Connect bank</p>
+        </Button>
       ) : (
-        <Button variant="ghost" asChild>
-          Connect bank
+        <Button onClick={() => open()} className="plaidlink-default">
+          <Image src="/icons/connect-bank.svg" alt="connect bank" width={24} height={24} />
+          <p className="text-[16px] font-semibold text-black-2">Connect bank</p>
         </Button>
       )}
     </>
