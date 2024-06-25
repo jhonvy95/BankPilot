453aa9169e0dc35996c95a25f0abcfd69ed75c49
jhon varon <jhonvaronyate@hotmail.com>
2024-06-25 13:16:25 -0500

    Transaction History Page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,7 +1,49 @@
+import HeaderBox from "@/components/HeaderBox";
+import TransactionsTable from "@/components/TransactionsTable";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const TransactionHistory = () => {
-  return <div>TransactionHistory</div>;
+const TransactionHistory = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+  return (
+    <div className="transactions">
+      <div className="transactions-header">
+        <HeaderBox title="Transaction History" subtext="See your bank details ans transactions." />
+      </div>
+      <div className="space-y-6">
+        <div className="transactions-account">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-18 font-bold text-white">{account?.data.name}</h2>
+            <p className="text-14 text-blue-25">{account?.data.officialName}</p>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              xxxx xxxx xxxx {account?.data.mask}
+            </p>
+          </div>
+
+          <div className="transactions-account-balance">
+            <p className="text-14">Current balance</p>
+            <p className="text-24 text-center font-bold">{account?.data.currentBalance}</p>
+          </div>
+        </div>
+
+        <section>
+          <TransactionsTable transactions={account?.transactions} />
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default TransactionHistory;
