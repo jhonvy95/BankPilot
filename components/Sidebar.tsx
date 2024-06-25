c1243f38f0bd129f3dff0474034038391ee31436
jhon varon <jhonvaronyate@hotmail.com>
2024-06-25 12:32:58 -0500

    Connect Multiple Bank Accounts

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const Sidebar = ({ user }: SiderbarProps) => {
   console.log(user);
@@ -52,6 +53,8 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
           );
         })}
+
+        <PlaidLink user={user} />
       </nav>
 
       <Footer user={user} />
